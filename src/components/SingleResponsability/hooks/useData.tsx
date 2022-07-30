1c8ae58f16a01753bbb024f525c1e8ab50f22933
Renszo D'Giovanni <varsovia84@gmail.com>
2022-07-30 20:00:38 +0200

    Create a custom hook to set the data

---
+++ src/components/SingleResponsability/hooks/useData.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+
+import { TodoType } from '../interfaces'
+import { fetchData } from '../services/fetchData'
+
+/**
+ * Just a hook to set the data
+ */
+
+export const useData = () => {
+  const [data, setData] = useState<TodoType[]>([])
+  const [isFetching, setIsFetching] = useState(true)
+
+  useEffect(() => {
+    fetchData()
+      .then(setData)
+      .catch(console.error)
+      .finally(() => setIsFetching(false))
+  }, [])
+
+  return {
+    data,
+    isFetching,
+    setIsFetching
+  }
+}
