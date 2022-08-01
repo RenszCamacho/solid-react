f544490f88d459bdb2827aa0b3e384d8277d9f22
Renszo D'Giovanni <varsovia84@gmail.com>
2022-08-01 12:18:42 +0200

    Give a trytach in the fetchData method

--- src/components/SingleResponsability/services/fetchData.ts
+++ src/components/SingleResponsability/services/fetchData.ts
@@ -7,7 +7,11 @@ const URL = 'https://jsonplaceholder.typicode.com/todos'
  * Just one responsability: fetch data from API
  */
 
-export const fetchData = async () => {
-  const response = await axios.get<TodoType[]>(URL)
-  return response.data
+export const fetchData = async (): Promise<TodoType[] | void> => {
+  try {
+    const response = await axios.get<TodoType[]>(URL)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
 }
